2b92147267a1a5492ceb0de00a6c3c6ceca38569
Monro, Ben <Ben.Monro@concur.com>
2018-04-01 20:39:42 -0700

    fix: lint fixes

--- examples/filter-validate-progress.js
+++ examples/filter-validate-progress.js
@@ -2,40 +2,41 @@
  * Filter and validate progress example
  */
 
-"use strict";
-var inquirer = require("..");
+'use strict';
+var inquirer = require('..');
 
 var questions = [
   {
-    type: "input",
-    name: "first_question",
-    message: "Question with filtering and validating text",
+    type: 'input',
+    name: 'first_question',
+    message: 'Question with filtering and validating text',
     validate: async () => {
-      await new Promise((r) => setTimeout(r, 3000));
+      await new Promise(r => setTimeout(r, 3000));
       return true;
     },
     filter: async answer => {
-      await new Promise((r) => setTimeout(r, 3000));
+      await new Promise(r => setTimeout(r, 3000));
       return `filtered${answer}`;
     },
-    filteringText: "Filtering your answer...",
-    validatingText: "Validating what you wrote..."
+    filteringText: 'Filtering your answer...',
+    validatingText: 'Validating what you wrote...'
   },
   {
-    type: "input",
-    name: "second_question",
-    message: "Question without filtering and validating text",
+    type: 'input',
+    name: 'second_question',
+    message: 'Question without filtering and validating text',
     validate: async () => {
-      await new Promise((r) => setTimeout(r, 3000));
+      await new Promise(r => setTimeout(r, 3000));
       return true;
     },
-    filter: async (answer) => {
-      await new Promise((r) => setTimeout(r, 3000));
+    filter: async answer => {
+      await new Promise(r => setTimeout(r, 3000));
       return `filtered${answer}`;
     }
   }
 ];
 
 inquirer.prompt(questions).then(answers => {
-  //...
+  console.log(answers);
+  // ...
 });
